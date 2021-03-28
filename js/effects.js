91efd0a888fc7dd62eedb1e0cbf2c98a14703952
FelipeB4C <felipeb4c@gmail.com>
2021-03-27 21:32:06 -0300

    Primeiro commit

---
+++ js/effects.js
@@ -0,0 +1,40 @@
+var login = document.getElementById("soundLogin");
+var success = document.getElementById("soundSuccess");
+
+var inptlogin = document.getElementById('login');
+var inptsenha = document.getElementById('senha');
+
+$(".login button").click(function(){
+  login.pause();
+  login.currentTime = 0;
+  login.play();
+  setTimeout(loginSucess, 1000*2)
+});
+
+function loginSucess() {
+
+  if(inptlogin.value === 'Red' && inptsenha.value === 'charizard'){
+    success.pause();
+    success.currentTime = 0;
+    success.play();
+    setTimeout(messageLogin, 1000*2)
+    
+    function messageLogin(){
+      alert("Login com sucesso! Bem-vindo de volta Red")
+    }
+
+  } else {
+    alert("Saia daqui equipe rocket!")
+  }
+  
+}
+
+function link1(){
+  alert("Problema é seu")
+
+}
+
+function link2(){
+  alert("Pra que quer caçar pokémon? É pra ficar em casa!")
+
+}
